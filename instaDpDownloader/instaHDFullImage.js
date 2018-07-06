9f0480c9afe6146f4cbf055c920450863cbabf04
tmibvishal <tmibvishal@gmail.com>
2018-07-06 09:30:12 +0530

    Modified files to handle error

--- instaDpDownloader/instaHDFullImage.js
+++ instaDpDownloader/instaHDFullImage.js
@@ -1,24 +1,33 @@
 let hdImageUrl;
 //this is pure java script
-function hdImage(username, func){
+function hdImage(username, funcsuccess, funcusernamenotfound){
     console.log("HD Insta Profile Image Url Fetching Started");
     callAjax(`https://apinsta.herokuapp.com/u/${username}`,function (data) {
+        console.log(data);
+        data = JSON.parse(data);
+
         let userid = data.graphql.user.id;
         callAjax(`https://i.instagram.com/api/v1/users/${userid}/info/`,function (data) {
+            data = JSON.parse(data);
             let hdimage = data.user.hd_profile_pic_url_info.url;
-            func(hdimage);
-        });
-    });
+            funcsuccess(hdimage);
+        },function(){});
+    },funcusernamenotfound);
     //return hdImageUrl;
 }
 
-function callAjax(url, callback){
+function callAjax(url, callback, funcusernamenotfound){
     var xmlhttp;
     // compatible with IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            callback(JSON.parse(xmlhttp.responseText));
+        if (xmlhttp.readyState == 4){
+            if(xmlhttp.status == 200){
+                callback(xmlhttp.responseText);
+            }
+            else{
+                funcusernamenotfound();
+            }
         }
     }
     xmlhttp.open("GET", url, true);
