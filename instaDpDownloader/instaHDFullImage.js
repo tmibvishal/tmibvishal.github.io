10dfb70444c314c41eeef8acea7a3edd1c39bd6b
tmibvishal <tmibvishal@gmail.com>
2018-07-09 14:55:04 +0530

    Revert "Modified files to handle error"
    This reverts commit 9f0480c9afe6146f4cbf055c920450863cbabf04.

--- instaDpDownloader/instaHDFullImage.js
+++ instaDpDownloader/instaHDFullImage.js
@@ -1,33 +1,24 @@
 let hdImageUrl;
 //this is pure java script
-function hdImage(username, funcsuccess, funcusernamenotfound){
+function hdImage(username, func){
     console.log("HD Insta Profile Image Url Fetching Started");
     callAjax(`https://apinsta.herokuapp.com/u/${username}`,function (data) {
-        console.log(data);
-        data = JSON.parse(data);
-
         let userid = data.graphql.user.id;
         callAjax(`https://i.instagram.com/api/v1/users/${userid}/info/`,function (data) {
-            data = JSON.parse(data);
             let hdimage = data.user.hd_profile_pic_url_info.url;
-            funcsuccess(hdimage);
-        },function(){});
-    },funcusernamenotfound);
+            func(hdimage);
+        });
+    });
     //return hdImageUrl;
 }
 
-function callAjax(url, callback, funcusernamenotfound){
+function callAjax(url, callback){
     var xmlhttp;
     // compatible with IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4){
-            if(xmlhttp.status == 200){
-                callback(xmlhttp.responseText);
-            }
-            else{
-                funcusernamenotfound();
-            }
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(JSON.parse(xmlhttp.responseText));
         }
     }
     xmlhttp.open("GET", url, true);
