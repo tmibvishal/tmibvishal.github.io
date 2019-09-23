d1d5d7cb69fffba9cdc639fd7a2f688578f0a851
Vishal Singh <tmibvishal@gmail.com>
2019-09-23 23:15:42 +0530

    minor2

--- vish/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    let circularimg = document.getElementById("circular-img");
-    circularimg.style.height = "200px";
-    circularimg.style.width = "200px";
-    circularimg.style.transform = "rotate(0deg)";
-}
